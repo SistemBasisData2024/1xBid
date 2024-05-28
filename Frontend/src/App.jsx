af9ac9483fcbbf5153eec1bcc0744701259052f2
unknown <darren06nb@gmail.com>
2024-05-28 18:56:36 +0700

    Adding Login and Register

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./pages/Profile";
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import { useEffect } from "react";
 import axios from "axios";
 
@@ -27,6 +29,8 @@ const App = () => {
       <Routes>
         <Route path="/" />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/signup" element={<Signup />} /> 
+        <Route path="/login" element={<Login />} /> 
         {/* Tambahkan rute lainnya di sini */}
       </Routes>
     </Router>
