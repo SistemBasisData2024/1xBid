cf9614ad0478c7c3732d3956b496d05d85a68aff
unknown <darren06nb@gmail.com>
2024-05-31 23:22:45 +0700

    toko details

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,26 +3,12 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./pages/Profile";
 import Signup from './pages/Signup';
 import Login from './pages/Login';
+import TokoDetails from './pages/TokoDetails';
 import { useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const username = "ten";
-        const password = "ten123";
-        const response = await axios.post("http://localhost:5000/auth/login", {
-          username,
-          password,
-        });
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    fetchData();
-  });
+
 
   return (
     <Router>
@@ -31,7 +17,7 @@ const App = () => {
         <Route path="/profile" element={<Profile />} />
         <Route path="/signup" element={<Signup />} /> 
         <Route path="/login" element={<Login />} /> 
-        {/* Tambahkan rute lainnya di sini */}
+        <Route path="/tokodetails" element={<TokoDetails />} /> 
       </Routes>
     </Router>
   );
--- Frontend/src/api/auth.handler.js
+++ Frontend/src/api/auth.handler.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
-import dotenv from 'dotenv';
-dotenv.config();
+
 
 export const registerHandler = async (username, email, fullname, password, date_of_birth, phone_number) => {
     try {
         if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)) throw new Error('Invalid email');
         if (username.match(/[^a-zA-Z0-9]/)) throw new Error('Invalid username');
         if (phone_number.match(/[^0-9]/) || phone_number.length > 12) throw new Error('Invalid phone number');
-        const response = await axios.post(process.env.BE_URL + '/auth/register', { username, email, fullname, password, date_of_birth, phone_number });
+        const response = await axios.post('localhost/5000' + '/auth/register', { username, email, fullname, password, date_of_birth, phone_number });
         return response.data;
     } catch (error) {
         console.error(error);
