3288a419266a5eb1f8bcde48a76fb1a8d1721c9d
MorpKnight <christoffelsihombing@gmail.com>
2024-05-31 19:27:33 +0700

    chore: Add logout functionality and update user authentication routes

--- Backend/src/controllers/Auth.controllers.js
+++ Backend/src/controllers/Auth.controllers.js
@@ -22,7 +22,7 @@ exports.login = async (req, res) => {
 
 exports.logout = async (req, res) => {
     try {
-        res.clearCookies('token');
+        res.clearCookie('token');
         res.status(200).json({ message: 'Logout successfully' });
     } catch (err) {
         res.status(400).json({ message: err.message });
--- Frontend/src/pages/ProfilePage.jsx
+++ Frontend/src/pages/ProfilePage.jsx
@@ -1,23 +1,30 @@
 import React, { useState, useEffect } from "react";
-import {
-  getUserHandler,
-  updateUserHandler,
-  addAddressHandler,
-  editAddressHandler,
-  topUpSaldoHandler,
-} from "@/api/user.handler";
-import { toast, ToastContainer } from "react-toastify";
+import { getUserHandler } from "@/api/user.handler";
 import ProfileTab from "@/components/profiles/profileTab";
 import AddressTab from "@/components/profiles/addressTab";
 import SaldoTab from "@/components/profiles/saldoTab";
-import Navbar from "@/components/navbar";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
   const [activeTab, setActiveTab] = useState(
     localStorage.getItem("activeTab") || "profile"
   );
   const [user, setUser] = useState({});
+  const [userAvatar, setUserAvatar] = useState("");
   const [address, setAddress] = useState([]);
+
+  const getAvatar = async (fullname) => {
+    if (fullname) {
+      const response = await fetch(
+        "https://ui-avatars.com/api/?name=" + fullname.replace(" ", "+")
+      );
+      const avatarUrl = await response.url;
+      return avatarUrl;
+    } else {
+      return "https://img.icons8.com/?size=100&id=99268&format=png&color=000000"
+    }
+  };
 
   useEffect(() => {
     const lastActiveTab = localStorage.getItem("activeTab");
@@ -32,8 +39,10 @@ const ProfilePage = () => {
         ).toLocaleDateString();
         setUser(response.user);
         setAddress(response.address);
+        const avatarUrl = await getAvatar(user.fullname);
+        setUserAvatar(avatarUrl);
       } else {
-        toast.error("Failed to fetch user data");
+        window.location.href = "/login";
       }
     };
     fetchUser();
@@ -46,17 +55,27 @@ const ProfilePage = () => {
 
   return (
     <div className="p-4">
-      <Navbar />
-      <div className="flex mt-16">
-        <img
-          src="profile.jpg"
-          alt="Profile"
-          className="w-32 h-32 rounded-full border-2 border-blue-500"
-        />
-        <div className="ml-4">
-          <h1 className="text-2xl font-bold text-blue-500">{user.fullname}</h1>
-          <p className="text-gray-600">{user.email}</p>
+      <div className="flex justify-between items-center mt-16">
+        <div className="flex items-center">
+          <img
+            src={userAvatar}
+            className="w-32 h-32 rounded-full border-2 border-blue-500"
+          />
+          <div className="ml-4">
+            <h1 className="text-2xl font-bold text-blue-500">
+              {user.fullname}
+            </h1>
+            <p className="text-gray-600">{user.email}</p>
+          </div>
         </div>
+        <a href="/logout">
+          <button
+            type="submit"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Logout
+          </button>
+        </a>
       </div>
 
       <div className="mt-4">
